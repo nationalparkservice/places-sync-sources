d3c62e28bb519a52de98cde9f5d75757cfe0ec44
Jim McAndrew <jim@loc8.us>
2016-06-02 13:18:11 -0600

    Add test for filters

---
+++ test/index.js
@@ -0,0 +1,23 @@
+var tools = require('jm-tools');
+var iterateTapeTasks = tools.iterateTapeTasks;
+var requireDirectory = tools.requireDirectory;
+
+var tests = requireDirectory(__dirname, 'index.js');
+var mainTaskList = [];
+
+console.log(tests);
+for (var test in tests) {
+  tests[test].forEach(function (test) {
+    mainTaskList.push(test);
+  });
+}
+
+iterateTapeTasks(mainTaskList, true, true, true).then(function (results) {
+  console.log('main tests done ');
+}).catch(function (e) {
+  if (e === undefined) {
+    e = new Error('undefined error ');
+  }
+  console.log('main test error ');
+  throw e;
+});
