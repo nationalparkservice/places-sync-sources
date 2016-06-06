bd0e29ba2d6a4ae3013ec64f9d7a51a32f664a43
Jim McAndrew <jim@loc8.us>
2016-06-06 12:26:17 -0600

    Clean up old code, add translation type

--- sources/openstreetmap.js
+++ sources/openstreetmap.js
@@ -1,7 +1,6 @@
 var Promise = require('bluebird');
 var template = require('./_template.js');
-var tools = require('jm-tools');
-var sources = tools.requireDirectory(__dirname + '/', [__filename, '_template.js', 'index.js']);
+// var tools = require('jm-tools')
 var superagent = require('superagent');
 var OAuth = require('oauth').OAuth;
 var columnsFromConfig = require('../helpers/columnsFromConfig');
@@ -11,56 +10,7 @@ var directReturn = function (_) {
   return _;
 };
 
-var parseColumns = function (inColumns) {
-  console.log('INCOLUMZ', inColumns);
-  return columnsFromConfig(inColumns);
-  /*
-  return columnsFromConfig([{
-    'name': 'key',
-    'type': 'text',
-    'sqliteType': 'text',
-    'defaultValue': '',
-    'notNull': true,
-    'sqliteColumnId': 0,
-    'primaryKey': 1
-  }, {
-    'name': 'members',
-    'type': 'text',
-    'sqliteType': 'text',
-    'defaultValue': '',
-    'notNull': true,
-    'sqliteColumnId': 2,
-    'primaryKey': 0
-  }, {
-    'name': 'foreign_key',
-    'type': 'text',
-    'sqliteType': 'text',
-    'defaultValue': '',
-    'notNull': true,
-    'sqliteColumnId': 3,
-    'primaryKey': 0
-  }, {
-    'name': 'source_tags',
-    'type': 'text',
-    'sqliteType': 'text',
-    'defaultValue': '',
-    'notNull': true,
-    'sqliteColumnId': 4,
-    'primaryKey': 0
-  }, {
-    'name': 'last_update',
-    'type': 'NUMERIC',
-    'sqliteType': 'NUMERIC',
-    'defaultValue': 0,
-    'notNull': true,
-    'sqliteColumnId': 5,
-    'primaryKey': 0
-  }]);
-  */
-};
-
 var createOauth = function (connection) {
-  console.log('***', connection, '***');
   return new OAuth(
     'http://' + connection.address + 'oauth/request_token',
     'http://' + connection.address + 'oauth/access_token',
@@ -85,16 +35,8 @@ var validateUser = function (connection, oauth) {
   });
 };
 
-var createCache = function (cacheConfig) {
-  return sources[cacheConfig.type](cacheConfig).then(function (source) {});
-};
-
-var loadCache = function (cacheConnectionConfig) {
-  return sources[cacheConnectionConfig];
-};
-
 var load = function (connection, columns) {
-  tasks = [{
+  var tasks = [{
     'name': 'OAuth',
     'description': 'Creates the oauth object',
     'task': createOauth,
@@ -107,7 +49,7 @@ var load = function (connection, columns) {
   }, {
     'name': 'columns',
     'description': 'columns',
-    'task': parseColumns,
+    'task': columnsFromConfig,
     'params': [columns]
   }, {
     'name': 'data',
@@ -120,20 +62,18 @@ var load = function (connection, columns) {
   return tasks;
 };
 
-var QuerySource = function (databaseConnection) {
-  return function (type, whereObj, returnColumns) {
-    returnColumns = returnColumns || columns;
-    var keys = columnsToKeys(returnColumns);
-    var createQueries = new CreateQueries(columns, keys.primaryKeys, keys.lastUpdatedField, keys.removedField);
-  // / TODO: ?
+var WriteFn = function (databaseConnection, connectionConfig, columns) {
+  return function () {
+    return new Promise(function (resolve, reject) {
+      // TODO, write stuff
+      resolve();
+    });
   };
 };
 
-var WriteFn = function (databaseConnection, connectionConfig, columns) {};
-
 // Prepare for the next release of places-sync-sources
 var osmSource = {
-  'QuerySource': QuerySource,
+  'QuerySource': undefined,
   'WriteFn': WriteFn,
   'defaultFields': {},
   'requiredFields': {
@@ -145,6 +85,7 @@ var osmSource = {
       'access_secret': 'string'
     },
     'columns': 'objectData',
+    'translation': 'objectData',
     'fields': 'objectNoData'
   },
   'load': load
