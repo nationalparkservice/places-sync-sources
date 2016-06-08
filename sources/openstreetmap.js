56e1821d4c2b28b904614e7ac1739531f5295619
Jim McAndrew <jim@loc8.us>
2016-06-08 08:05:16 -0600

    Update this whole function

--- sources/openstreetmap.js
+++ sources/openstreetmap.js
@@ -1,9 +1,12 @@
 var Promise = require('bluebird');
 var template = require('./_template.js');
-// var tools = require('jm-tools')
+var tools = require('jm-tools');
+var translator;
+var columnsToKeys = require('../helpers/columnsToKeys');
 var superagent = require('superagent');
 var OAuth = require('oauth').OAuth;
 var columnsFromConfig = require('../helpers/columnsFromConfig');
+var osmSubmit = require('openstreetmap-submit');
 require('superagent-oauth')(superagent);
 
 var directReturn = function (_) {
@@ -62,11 +65,164 @@ var load = function (connection, columns) {
   return tasks;
 };
 
-var WriteFn = function (databaseConnection, connectionConfig, columns) {
-  return function () {
-    return new Promise(function (resolve, reject) {
-      // TODO, write stuff
-      resolve();
+var createSource = function (data, translationType, columns) {
+  var sourceTemplate = {
+    'name': tools.guid(),
+    'connection': {
+      'data': [],
+      'type': 'json',
+      'translationType': 'generic'
+    },
+    'columns': [],
+    'fields': {
+      'primaryKey': []
+    }
+  };
+  var returnValue = JSON.parse(JSON.stringify(sourceTemplate));
+  returnValue.connection.data = data;
+  returnValue.connection.translationType = translationType;
+  returnValue.columns = columns;
+  columns.forEach(function (column) {
+    var v = column.primaryKey;
+    if (!(v === undefined || v === null || v === false)) {
+      returnValue.fields.primaryKey.push(column.name);
+    }
+  });
+  return returnValue;
+};
+
+var matchKeys = function (updated, removed, metadata, keys) {
+  // Create the template that this will return
+  var changes = {
+    'create': [],
+    'modify': [],
+    'remove': [],
+    'possible remove': []
+  };
+
+  var cache = {};
+
+  // Add all the updates to the cache as "create"
+  updated.forEach(function (record) {
+    // Create a key for this updated event
+    var key = keys.primaryKeys.map(function (pk) {
+      return record[pk];
+    }).join(',');
+    cache[key] = {
+      'type': 'create',
+      'data': record,
+      'foreignKey': null
+    };
+  });
+
+  // If there's a remove and a create for a record, that's really a replace
+  // So we only remove what we don't have a create for
+  // We mark it as a possible remove, because if it's not in OSM, we don't remove it at all
+  removed.forEach(function (record) {
+    // Create a key for this updated event
+    var key = keys.primaryKeys.map(function (pk) {
+      return record[pk];
+    }).join(',');
+    if (!cache[key]) {
+      cache[key] = {
+        'type': 'possible remove',
+        'data': record,
+        'foreignKey': null
+      };
+    }
+  });
+
+  // Go through the metadata
+  // If a create already exists in OSM, it's really a modify
+  // If a possible remove exists in OSM, it's really a remove
+  metadata.forEach(function (record) {
+    var key = keys.primaryKeys.map(function (pk) {
+      return record[pk];
+    }).join(',');
+    if (cache[key]) {
+      if (cache[key].type === 'create') {
+        cache[key].type = 'modify';
+        cache[key].foreignKey = record.foreignKey;
+      } else if (cache[key].type === 'possible remove') {
+        cache[key].type = 'remove';
+        cache[key].foreignKey = record.foreignKey;
+      }
+    }
+  });
+
+  // Loop through the cache and add the osmid to the records
+  Object.keys(cache).forEach(function (key) {
+    cache[key].data['_osm_id'] = cache[key].foreignKey;
+    changes[cache[key].type].push(cache[key].data);
+  });
+
+  // We don't need the possible removes, so lets take them out of memory
+  delete changes['possible remove'];
+
+  return changes;
+};
+
+var WriteFn = function (databaseConnection, connectionConfig, columns, immutableConfig) {
+  // Extract the translation type
+  var translationType = immutableConfig.translation.toJS().join('');
+  var keys = columnsToKeys(columns);
+
+  return function (updated, removed, metadata) {
+    // Define the translator only when it's called to prevent circular requires
+    translator = translator || require('places-sync-translator');
+
+    // Determine the created/modified/deleted data
+    var changesObj = matchKeys(updated, removed, metadata, keys);
+
+    // Create the translation tasks for created/modified/deleted data
+    var changesObjKeys = [];
+    var tasks = [];
+    Object.keys(changesObj).forEach(function (key) {
+      if (changesObj[key].length) {
+        changesObjKeys.push(key);
+        // console.log(createSource(changesObj[key], translationType, columns))
+        tasks.push(translator(createSource(changesObj[key], translationType, columns)));
+      }
+    });
+
+    // also throw in a task to create the osm-submit object at the end
+    var osmSubmitOptions = {
+      'primaryKey': keys.primaryKeys[0],
+      'lastEdit': keys.lastUpdatedField,
+      'limit': 15
+    };
+
+    tasks.push(osmSubmit({
+      'connection': connectionConfig
+    },
+      osmSubmitOptions
+    ));
+
+    return Promise.all(tasks).then(function (results) {
+      // Map the result items back out to an object to make them easier to deal with
+      var translatedObj = {};
+      changesObjKeys.map(function (key, i) {
+        translatedObj[key] = results[i];
+      });
+      var submit = results[results.length - 1];
+      var dummyGeoJson = {
+        'features': []
+      };
+
+      // Run the submit!
+      return submit(translatedObj.create || dummyGeoJson, translatedObj.modify || dummyGeoJson, translatedObj.remove || dummyGeoJson).then(function (submitResult) {
+        console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
+        console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
+        console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
+        console.log(JSON.stringify(submitResult, null, 2));
+        console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
+        console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
+        console.log('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
+        return {
+          'updated': updated,
+          'removed': removed
+        };
+      });
     });
   };
 };
