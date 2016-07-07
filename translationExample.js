670e13a4be6218212cd7aaaaad1ebf038eab4561
Jim McAndrew <jim@loc8.us>
2016-07-07 07:15:32 -0600

    better example

--- translationExample.js
+++ translationExample.js
@@ -1,5 +1,6 @@
 var translator = require('places-sync-translator');
 var example = module.exports = function (source, type) {
+  source.connection.translationType = source.connection.translationType || type;
   return translator(source, type).then(function (translatedGeoJson) {
     return translatedGeoJson;
   }).catch(function (err) {
@@ -11,13 +12,14 @@ var testSource = {
   'name': 'Generic ArcGIS',
   'connection': {
     'type': 'arcgis',
-    'url': 'http://services1.arcgis.com/fBc8EJBxQRMcHlei/ArcGIS/rest/services/BLCACURE/FeatureServer/0'
+    'url': 'http://services1.arcgis.com/fBc8EJBxQRMcHlei/ArcGIS/rest/services/BLCACURE/FeatureServer/13'
   },
   'fields': {
     'primaryKey': 'GlobalID'
   }
 };
 
-// example(testSource, 'poi').then(function(){
-  // console.log('done');
-// });
+example(testSource, 'parkinglots').then(function (r) {
+  console.log('done');
+  console.log(JSON.stringify(JSON.parse(r), null, 2));
+});
